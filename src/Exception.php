21128748e2cf99393d7e6225e5fe35c9f6605f88
KidsReturn <leonsw.com@gmail.com>
2019-08-06 19:23:25 +0800

    fix

---
+++ src/Exception.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Leonsw\Http;
+
+class Exception extends \Exception
+{
+    /**
+     * @var int HTTP status code, such as 403, 404, 500, etc.
+     */
+    public $statusCode;
+
+
+    /**
+     * Constructor.
+     * @param int $status HTTP status code, such as 404, 500, etc.
+     * @param string $message error message
+     * @param int $code error code
+     * @param \Exception $previous The previous exception used for the exception chaining.
+     */
+    public function __construct($status, $message = null, $code = 0, \Exception $previous = null)
+    {
+        $this->statusCode = $status;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return StatusMap::reason($this->statusCode);
+    }
+}
